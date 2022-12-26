876ad26aa10921b4303fbd1aaf530c5f1f6073c0
henok8515 <henokegezew44@gmail.com>
2022-12-26 11:40:04 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  let num: Number = 10;
+  num = 20;
+  console.log(num);
+  return <div>{num}</div>;
+}
+
+export default App;
---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: { main: "#fff149" },
+  },
+});
+export default theme;
